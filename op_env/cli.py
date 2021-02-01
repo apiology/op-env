45c26e61cd887261114e30633a76290394410df8
Vince Broz <vince@broz.cc>
2021-02-01 13:32:21 -0500

    Teach argument parsing about the 'run' command

--- op_env/cli.py
+++ op_env/cli.py
@@ -3,11 +3,22 @@ import argparse
 import sys
 
 
-def main():
-    """Console script for op_env."""
+def parse_argv(argv):
     parser = argparse.ArgumentParser()
-    parser.add_argument('_', nargs='*')
-    args = parser.parse_args()
+    subparsers = parser.add_subparsers(help='Run the specified command with the given environment variables', dest='subparser_name')
+    run_parser = subparsers.add_parser('run')
+    run_parser.add_argument('-e',
+                            metavar='ENVVAR',
+                            help='environment variable name to set, based on item with same tag in 1Password')  # TODO help should show something useful
+    run_parser.add_argument('command',
+                            nargs='+',
+                            help='Command to run with the environment set from 1Password')
+    return parser.parse_args(argv[1:])
+
+
+def main(argv=sys.argv):
+    """Console script for op_env."""
+    args = parse_argv(argv)
 
     print("Arguments: " + str(args._))
     print("Replace this message by putting your code into "
--- tests/test_op_env.py
+++ tests/test_op_env.py
@@ -5,7 +5,7 @@
 import pytest
 import subprocess
 
-# from op_env import op_env
+from op_env.cli import parse_argv
 
 
 @pytest.fixture
@@ -23,17 +23,51 @@ def test_content(response):
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
 
+def test_parse_args_run_commadn_with_arguments_():
+    argv = ['op-env', 'run', '-e', 'DUMMY', 'mycmd', '1', '2', '3']
+    args = parse_argv(argv)
+    assert vars(args) == {'command': ['mycmd', '1', '2', '3'], 'e': 'DUMMY', 'subparser_name': 'run'}
 
-def test_cli_help():
-    expected_help = """usage: op-env [-h] [_ ...]
+def test_parse_args_run_simple():
+    argv = ['op-env', 'run', '-e', 'DUMMY', 'mycmd']
+    args = parse_argv(argv)
+    assert vars(args) == {'command': ['mycmd'], 'e': 'DUMMY', 'subparser_name': 'run'}
+
+
+@pytest.mark.skip(reason="not yet written")
+def test_cli_run():
+    argv = ['op-env', 'run', '-e', 'DUMMY', 'env']
+    expected_envvar = 'DUMMY=dummyvalue'
+    actual_output = subprocess.check_output(argv).decode('utf-8')
+    assert expected_envvar in actual_output
+
+
+def test_cli_help_run():
+    expected_help = """usage: op-env run [-h] [-e ENVVAR] command [command ...]
 
 positional arguments:
-  _
+  command     Command to run with the environment set from
+              1Password
+
+optional arguments:
+  -h, --help  show this help message and exit
+  -e ENVVAR   environment variable name to set, based on item with
+              same tag in 1Password
+"""
+    # older python versions show arguments like this:
+    actual_help = subprocess.check_output(['op-env', 'run', '--help']).decode('utf-8')
+    assert actual_help == expected_help
+
+def test_cli_help():
+    expected_help = """usage: op-env [-h] {run} ...
+
+positional arguments:
+  {run}       Run the specified command with the given environment
+              variables
 
 optional arguments:
   -h, --help  show this help message and exit
 """
     # older python versions show arguments like this:
-    alt_expected_help = expected_help.replace('[_ ...]', '[_ [_ ...]]')
     actual_help = subprocess.check_output(['op-env', '--help']).decode('utf-8')
-    assert actual_help in [expected_help, alt_expected_help]
+    assert actual_help == expected_help
