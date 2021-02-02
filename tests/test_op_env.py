55e2a9ed1cd7956e9145429745f5c7384630fd64
Vince Broz <vince@broz.cc>
2021-02-02 09:12:34 -0500

    Small cleanups of some remaining boilerplate code

--- tests/test_op_env.py
+++ tests/test_op_env.py
@@ -11,22 +11,6 @@ from op_env.cli import parse_argv
 from op_env.op import op_lookup
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
-
-
 def test_op_lookup():
     with patch('op_env.op.subprocess') as mock_subprocess:
         op_lookup('TEST_VALUE')
