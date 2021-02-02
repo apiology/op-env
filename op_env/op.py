6dc6087c2cb18bff0452e13bdaa1e142d890672c
Vince Broz <vince@broz.cc>
2021-02-02 10:39:29 -0500

    Avoid hard-coding arguments to op list items

--- op_env/op.py
+++ op_env/op.py
@@ -3,7 +3,7 @@ import subprocess
 
 def op_lookup(env_var_name: str) -> str:
     # https://stackoverflow.com/questions/13332268/how-to-use-subprocess-command-with-pipes
-    list_command = ['op', 'list', 'items', '--tags', 'TEST_VALUE']
+    list_command = ['op', 'list', 'items', '--tags', env_var_name]
     pipe = subprocess.Popen(list_command,
                             stdout=subprocess.PIPE)
     get_command = ['op', 'get', 'item', '-', '--fields', 'password']
