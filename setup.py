61ecddc672b4f17f26402b9dfb05f6baeea1bc1f
Vince Broz <vince@broz.cc>
2021-02-11 16:05:21 -0500

    Bump version to 0.3.0

--- setup.py
+++ setup.py
@@ -116,7 +116,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/apiology/op_env',
-    version='0.2.0',
+    version='0.3.0',
     zip_safe=False,
     cmdclass={
         'quality': QualityCommand,
