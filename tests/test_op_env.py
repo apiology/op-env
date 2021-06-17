29d01274f445dd52d4be56b2b242be1c503690d6
Vince Broz <vince@broz.cc>
2021-06-17 13:37:01 -0400

    Update template

--- tests/test_op_env.py
+++ tests/test_op_env.py
@@ -10,7 +10,7 @@ from unittest.mock import call, patch
 
 import pytest
 
-from op_env.cli import parse_argv, process_args
+from op_env.cli import main, parse_argv, process_args
 
 
 @pytest.fixture
@@ -29,16 +29,16 @@ def test_content(response):
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
 
 
-def test_process_args():
-    with patch('builtins.print') as mock_print:
-        ns = argparse.Namespace()
-        setattr(ns, '_', '<fake>')
-        out = process_args(ns)
+@patch('builtins.print', autospec=print)
+def test_process_args(print):
+    ns = argparse.Namespace()
+    setattr(ns, 'foo', '<fake>')
+    out = process_args(ns)
 
-        assert out == 0
-        mock_print.assert_has_calls([call('Arguments: <fake>'),
-                                     call('Replace this message by putting '
-                                          'your code into op_env.cli.process_args')])
+    assert out == 0
+    print.assert_has_calls([call("Arguments: Namespace(foo='<fake>')"),
+                            call('Replace this message by putting '
+                                 'your code into op_env.cli.process_args')])
 
 
 # @pytest.mark.skip(reason="working on main help test first")
@@ -46,6 +46,15 @@ def test_parse_argv_run_simple():
     argv = ['op_env', 'op1', '123']
     args = parse_argv(argv)
     assert vars(args) == {'operation': 'op1', 'arg1': 123}
+
+
+@patch('op_env.cli.parse_argv', autospec=parse_argv)
+@patch('op_env.cli.process_args', autospec=process_args)
+def test_main(process_args, parse_argv):
+    argv = object()
+    args = parse_argv.return_value
+    assert process_args.return_value == main(argv)
+    process_args.assert_called_with(args)
 
 
 def test_cli_op1_help():
@@ -66,6 +75,23 @@ optional arguments:
     # older python versions show arguments like this:
     actual_help = subprocess.check_output(['op_env', 'op1', '--help'],
                                           env=env).decode('utf-8')
+    assert actual_help == expected_help
+
+
+def test_cli_no_command():
+    request_long_lines = {'COLUMNS': '999', 'LINES': '25'}
+    env = {}
+    env.update(os.environ)
+    env.update(request_long_lines)
+    expected_help = """usage: op_env [-h] {op1} ...
+op_env: error: Please provide a command
+"""
+    # older python versions show arguments like this:
+    result = subprocess.run(['op_env'],
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE,
+                            env=env)
+    actual_help = result.stderr.decode('utf-8')
     assert actual_help == expected_help
 
 
