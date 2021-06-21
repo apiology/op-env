6dbccbd6a9f98e0b40b7d6391b29ca99bc6b33aa
Vince Broz <vince@broz.cc>
2021-06-21 16:42:34 -0400

    Bump version: 0.7.0 → 0.7.1

--- setup.py
+++ setup.py
@@ -134,6 +134,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/apiology/op_env',
-    version='0.7.0',
+    version='0.7.1',
     zip_safe=False,
 )
