84269db5ca58801bff3ea4f0ab56b6ac74132806
Vince Broz <vince@broz.cc>
2021-10-20 12:48:22 -0400

    Bump version: 0.8.0 → 0.9.0

--- op_env/__init__.py
+++ op_env/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Vince Broz"""
 __email__ = 'vince@broz.cc'
-__version__ = '0.8.0'
+__version__ = '0.9.0'
--- setup.py
+++ setup.py
@@ -134,6 +134,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/apiology/op_env',
-    version='0.8.0',
+    version='0.9.0',
     zip_safe=False,
 )
